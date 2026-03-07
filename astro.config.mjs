2ef2e90094c7b61f67229db3c9a25e76a9966d62
GGM <ggm.work22@gmail.com>
2026-03-07 12:31:17 +0000

    wip: applying project changes to feature-testing branch

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,10 @@
 import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
-import image from "@astrojs/image";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   site: process.env.SITE_URL,
-  integrations: [sitemap(), image()],
+  integrations: [sitemap()],
   vite: {
     plugins: [tailwindcss()],
   },
